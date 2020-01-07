aed26b462eebe9e5e41d2e7171bcf446d9245e63
Sai Santhosh <santhoshteen@gmail.com>
2020-01-08 02:38:19 +0530

    Updates

---
+++ precache-manifest.920dc0b0636bb9af2809a7ca016d63f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0abb19d27579333b6a1b5d8ff79ab2",
+    "url": "/react-crud-app/index.html"
+  },
+  {
+    "revision": "79b5fa4852fc2a645504",
+    "url": "/react-crud-app/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "d410fc4719cdf4d996af",
+    "url": "/react-crud-app/static/css/main.dabba314.chunk.css"
+  },
+  {
+    "revision": "79b5fa4852fc2a645504",
+    "url": "/react-crud-app/static/js/2.daa4985f.chunk.js"
+  },
+  {
+    "revision": "adc4fd9088f875ffd322d38c39ba71de",
+    "url": "/react-crud-app/static/js/2.daa4985f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d410fc4719cdf4d996af",
+    "url": "/react-crud-app/static/js/main.663ca995.chunk.js"
+  },
+  {
+    "revision": "13fbf02aea9ec777ed64",
+    "url": "/react-crud-app/static/js/runtime-main.accd64c1.js"
+  }
+]);
